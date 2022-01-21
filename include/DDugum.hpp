14d0dfc255a63c8c6907efca95d71dda067402fd
Tea Shkurti <72191797+Tea01@users.noreply.github.com>
2022-01-21 23:07:38 +0300

    Update DDugum.hpp

--- include/DDugum.hpp
+++ include/DDugum.hpp
@@ -1,11 +1,4 @@
-/** 
-* @file                         B191210555
-* @description                  .txt dosyadan verileri okuyup istenilen indisten ekleme ve silme işlemi yaparak ekrana verileri yazdırır(iki yönlü bağlı liste kullanarak)
-* @course                       1. Öğretim C Grubu
-* @assignment                   1. Ödev
-* @date                         12/11/2021
-* @author                       Tea Shkurti | E-posta: tea.shkurti@ogr.sakarya.edu.tr
-*/
+
 
 #ifndef DDugum_hpp
 #define DDugum_hpp
