f7ea86fb96549020bd6c430795b86cab42eed409
Tea Shkurti <72191797+Tea01@users.noreply.github.com>
2022-01-21 23:06:55 +0300

    Update Liste.cpp

--- src/Liste.cpp
+++ src/Liste.cpp
@@ -1,11 +1,3 @@
-/** 
-* @file                         B191210555
-* @description                  .txt dosyadan verileri okuyup istenilen indisten ekleme ve silme işlemi yaparak ekrana verileri yazdırır(iki yönlü bağlı liste kullanarak)
-* @course                       1. Öğretim C Grubu
-* @assignment                   1. Ödev
-* @date                         12/11/2021
-* @author                       Tea Shkurti | E-posta: tea.shkurti@ogr.sakarya.edu.tr
-*/
 
 #include "Liste.hpp"
 #include <iostream>
